7f286336f7705d36337168e2637ea33134d2924a
RaCuNi <RaCuNi@outlook.kr>
2016-02-29 20:26:49 +0900

    fix message error and add messenger command, raonitem command.

--- src/main/java/io/github/RaCuNi/RACUNIONLINE/Main.java
+++ src/main/java/io/github/RaCuNi/RACUNIONLINE/Main.java
@@ -1,8 +1,13 @@
 package io.github.RaCuNi.RACUNIONLINE;
 
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.Sound;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -10,22 +15,51 @@ public class Main extends JavaPlugin{
 	
 	public void onEnable(){
 		PluginDescriptionFile pdf = getDescription();
-		this.getLogger().info(pdf.getName() + pdf.getVersion() + "has been enabled!");
+		this.getLogger().info(pdf.getName() + " " + pdf.getVersion() + " has been enabled!");
 	}
 	
 	public void onDisable(){
 		PluginDescriptionFile pdf = getDescription();
-		this.getLogger().info(pdf.getName() + pdf.getVersion() + "has been disabled!");
+		this.getLogger().info(pdf.getName() + " " + pdf.getVersion() + " has been disabled!");
 	}
 	
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args){
 		if(cmd.getName().equalsIgnoreCase("shutdown")){
 			Bukkit.shutdown();
-			getLogger().info(sender.getName() + "used shutdown command!");
-			Bukkit.broadcastMessage(sender.getName() + "used shutdown command!");
+			getLogger().info(sender.getName() + " used shutdown command!");
+			Bukkit.broadcastMessage(sender.getName() + " used shutdown command!");
 			return true;
+		}
+		else if(cmd.getName().equalsIgnoreCase("messenger")){
+			getLogger().info(sender+" used messenger!");
+			Bukkit.broadcastMessage(ChatColor.GOLD+"["+ChatColor.BLUE+sender+ChatColor.GOLD+"]" + " " + ChatColor.COLOR_CHAR + args[0]);
+			return true;
+		}
+		else if(cmd.getName().equalsIgnoreCase("raonitem")){
+			if (!(sender instanceof Player)) {
+				sender.sendMessage("This command can only run by a player.");
+			} else {
+				Player player = (Player) sender;
+				ItemStack leatherhelm = new ItemStack(Material.LEATHER_HELMET);
+				ItemStack leatherchestplate = new ItemStack(Material.LEATHER_CHESTPLATE);
+				ItemStack leatherleggings = new ItemStack(Material.LEATHER_LEGGINGS);
+				ItemStack leatherboots = new ItemStack(Material.LEATHER_BOOTS);
+				ItemStack ironsword = new ItemStack(Material.IRON_SWORD);
+				ItemStack ironaxe = new ItemStack(Material.IRON_AXE);
+				ItemStack ironpickaxe = new ItemStack(Material.IRON_PICKAXE);
+				ItemStack ironspade = new ItemStack(Material.IRON_SPADE);
+				ItemStack milk = new ItemStack(Material.MILK_BUCKET);
+				player.getInventory().setHelmet(leatherhelm);
+				player.getInventory().setChestplate(leatherchestplate);
+				player.getInventory().setLeggings(leatherleggings);
+				player.getInventory().setBoots(leatherboots);
+				player.playSound(player.getLocation(), Sound.LEVEL_UP, 2, 3);
+				player.getInventory().addItem(ironsword, ironaxe, ironpickaxe, ironspade, milk);
+				player.playSound(player.getLocation(), Sound.ITEM_PICKUP, 2, 3);
+				getLogger().info("Raon First Item given to " + ChatColor.GOLD + player);
+				player.sendMessage(ChatColor.GOLD + "Ra"+ ChatColor.BLUE + "on" + " " + ChatColor.GREEN + "First Item" + " " + ChatColor.MAGIC + "HOLIC" + ChatColor.YELLOW + "has given!");
+			}
 		}
 		return false;
 	}
-
 }
